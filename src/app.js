18eac34aec5d2e546d6017cf6810ce9afac3c05c
Wanda Antoine <wanda@thinkful.com>
2020-08-06 20:41:40 -0400

    prepared app for deploy

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
+const morganOption = (process.env.NODE_ENV === 'production')
     ? 'tiny'
     : 'common';
 
@@ -20,10 +20,9 @@ app.get('/', (req, res) => {
 
 app.use(function errorHandler(error, req, res, next) {
     let response
-    if (NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === 'production') {
         response = { error: { message: 'server error' } }
     } else {
-        console.error(error)
         response = { message: error.message, error }
     }
     res.status(500).json(response)
